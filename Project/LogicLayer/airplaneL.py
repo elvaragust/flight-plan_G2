8d649e0b99e2682bf2d859b66d02d430ecb42d9e
ThcraXR <thora23@ru.is>
2023-12-12 11:36:04 +0000

    s

--- Project/LogicLayer/airplaneL.py
+++ Project/LogicLayer/airplaneL.py
@@ -9,9 +9,9 @@ class AirplaneL:
         self.data_wrapper.create_airplane(airplane_info)
 
     def edit_airplane_info(self, airplane_name, update_info):
-        if not self.data_wrapper.get_airplane(airplane_name):
+        if not self.data_wrapper.get_airplane_info(airplane_name):
             return False
         return self.data_wrapper.edit_airplane(airplane_name, update_info)
 
     def get_plane_info(self, airplane_name):
-        return self.airplanes.get_airplane(airplane_name)
+        return self.airplanes.get_airplane_info(airplane_name)
