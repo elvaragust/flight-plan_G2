e3882542dbc95f4440051abaa587c7b147f3d8f5
elvaragust <elvarbesti@gmail.com>
2023-12-15 20:28:42 +0000

    data/logicwrapper completed

--- Project/DataLayer/employeeD.py
+++ Project/DataLayer/employeeD.py
@@ -6,24 +6,24 @@ class EmployeeData:
     FILE_NAME = "dataLayer\\employee.csv"
 
     def __init__(self):
-        self.load_data_from_file()
+        pass
         
-    def load_data_from_file(self):
-        with open(self.FILE_NAME, newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            next(reader)  # Skips the header row
-            for row in reader:
-                social_security, *employee_info = row
-                employee = Employees(social_security, *employee_info)
-                self.employees[social_security] = employee
+#    def load_data_from_file(self):
+#        with open(self.FILE_NAME, newline='') as csvfile:
+#            reader = csv.reader(csvfile)
+#            next(reader)  # Skips the header row
+#            for row in reader:
+#                social_security, *employee_info = row
+#                employee = Employees(social_security, *employee_info)
+#                self.employees[social_security] = employee
 
     def get_all_employees(self):
-        employees = []
+        employee_list = []
         with open(self.FILE_NAME, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                employees.append(row)  # append the row directly
-        return employees
+                employee_list.append(row)
+        return employee_list
             
     def save_data_to_file(self):
         with open(self.FILE_NAME, 'a', newline='') as csvfile:
@@ -38,5 +38,4 @@ class EmployeeData:
     def get_employee_by_id(self, social_security):
         return self.employees.get(social_security, None)
 
-    def get_employee_list(self):
-        return list(self.employees.values())
+
--- Project/LogicLayer/airplaneL.py
+++ Project/LogicLayer/airplaneL.py
@@ -1,16 +1,20 @@
 from dataLayer.logic_data_wrapper import LogicDataWrapper
+#from models.airplaneM import Airplanes
 
 class AirplaneL:
-    def __init__(self, data_wrapper):
+    def __init__(self):
         self.data_wrapper = LogicDataWrapper()
 
-    def register_airplane(self, airplane_info):
-        self.data_wrapper.create_airplane(airplane_info)
-
-    def edit_airplane_info(self, airplane_name, update_info):
-        if not self.data_wrapper.get_airplane_info(airplane_name):
-            return False
-        return self.data_wrapper.edit_airplane(airplane_name, update_info)
-
+    def create_airplane(self, airplane):
+        self.data_wrapper.create_airplane(airplane)
+        
     def get_plane_info(self, airplane_name):
-        return self.airplanes.get_airplane_info(airplane_name)
+        return self.data_wrapper.get_airplane_info(airplane_name)
+
+    def get_airplane_list(self):
+        return self.data_wrapper.get_airplane_list()
+
+#    def edit_airplane_info(self, airplane_name, update_info):
+#        if not self.data_wrapper.get_airplane_info(airplane_name):
+#            return False
+#        return self.data_wrapper.edit_airplane(airplane_name, update_info)
